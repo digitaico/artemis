f7012fea7d6bef561d913d8c29e3b22f507bd9d2
Jorge Eduardo Ardila <jea.data@gmail.com>
2023-08-14 10:26:07 -0500

    data preload async

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,20 @@
 const http = require('http');
 
 const app = require('./app');
-const server = http.createServer(app);
+
+const {loadPlanetsData} = require('./models/planets.model');
 
 const PORT = process.env.API_PORT || 8000;
 
-server.listen(PORT, () => {
-  console.log(`[Info] : Server UP and running ${PORT}`);
-})
+const server = http.createServer(app);
+
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`[Info] : Server UP and running ${PORT}`);
+  })
+}
+
+startServer();
+
