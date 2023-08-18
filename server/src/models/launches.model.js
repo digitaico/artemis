6fa81ef972a66f29fa765203faf7d3b10c56f561
Jorge Eduardo Ardila <jea.data@gmail.com>
2023-08-17 20:31:55 -0500

    crud: delete

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,13 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
-  destination: 'Kepler-442 b',
+  target: 'Kepler-442 b',
   customers: ['DIGITAI', 'NASA', 'ZTM'],
   upcoming: true,
   success: true,
@@ -13,10 +15,37 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+};
+
+
 function getAllLaunches() {
   return [...launches.values()];
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    launch.flightNumber,
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+      upcoming: true,
+      success: true,
+    })
+  );
+};
+
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.success = false;
+  aborted.upcoming = false;
+  return aborted;
+}
+
 module.exports = {
+  existsLaunchWithId,
   getAllLaunches,
+  addNewLaunch,
+  abortLaunchById,
 }
