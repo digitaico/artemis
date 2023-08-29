a8eac0b7a3254a4881a865869c47543ef3d0f841
Jorge Eduardo Ardila <jea.data@gmail.com>
2023-08-28 19:47:41 -0500

    migrated to mongoose

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,53 +1,70 @@
-//const launches = new Map();
-const launches = require('./launches.mongo');
+const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
 
-/*
-let latestFlightNumber = 100;
-const launch = {
-  flightNumber: 100,
-  mission: 'Kepler Exploration X',
-  rocket: 'Explorer IS1',
-  launchDate: new Date('December 27, 2030'),
-  target: 'Kepler-442 b',
-  customers: ['DIGITAI', 'NASA', 'ZTM'],
-  upcoming: true,
-  success: true,
+const DEFAULT_FLIGHT_NUMBER = 100;
+
+async function existsLaunchWithId(launchId) {
+  return await launchesDatabase.findOne({
+    flightNumber: launchId,
+  });
 };
 
-launches.set(launch.flightNumber, launch);
-*/
-
-function existsLaunchWithId(launchId) {
-  return launches.has(launchId);
-};
-
-
-function getAllLaunches() {
-  return [...launches.values()];
+async function getAllLaunches() {
+  return await launchesDatabase.find({}, {
+    '_id': 0,
+    '__v': 0,
+  });
 }
 
-function addNewLaunch(launch) {
-  latestFlightNumber++;
-  launches.set(
-    launch.flightNumber,
-    Object.assign(launch, {
-      flightNumber: latestFlightNumber,
-      upcoming: true,
-      success: true,
-    })
-  );
+async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error('No matching planet found!');
+  }
+
+  await launchesDatabase.findOneAndUpdate({
+    flightNumber: launch.flightNumber,
+  }, launch, {
+    upsert: true,
+  });
 };
 
-function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId);
-  aborted.success = false;
-  aborted.upcoming = false;
-  return aborted;
+async function getLatestFlightNumber() {
+  const latestLaunch = await launchesDatabase
+    .findOne()
+    .sort('-flightNumber');
+
+  return (!latestLaunch) ? DEFAULT_FLIGHT_NUMBER : latestLaunch.flightNumber;
+}
+
+async function scheduleNewLaunch(launch) {
+  const newFlightNumber = await getLatestFlightNumber() + 1;
+  const newLaunch = Object.assign(launch, {
+    flightNumber: newFlightNumber,
+    success: true,
+    upcoming: true,
+    customers: ['DIGITAI', 'ADVICOM', 'DATELLIGENCE'],
+  });
+  await saveLaunch(newLaunch);
+}
+
+async function abortLaunchById(launchId) {
+  const aborted = await launchesDatabase.updateOne({
+    flightNumber: launchId,
+  }, {
+    upcoming: false,
+    success: false,
+  });
+
+  return aborted.modifiedCount === 1;
 }
 
 module.exports = {
   existsLaunchWithId,
   getAllLaunches,
-  addNewLaunch,
+  scheduleNewLaunch,
   abortLaunchById,
 }
